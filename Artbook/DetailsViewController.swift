07556c92acde08b8b6724b7e5a7877eeb61f3d8c
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-11 17:33:26 +0300

    Add elements to the code

--- Artbook/DetailsViewController.swift
+++ Artbook/DetailsViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldArtist: UITextField!
+    @IBOutlet weak var textFieldYear: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButton(_ sender: Any) {
     }
-    */
-
+    
 }
